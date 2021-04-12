a8c1d117c62f21c491253645e211ccfe35983bca
Jotak1 <jp.ausensi@gmail.com>
2021-04-12 15:12:25 -0400

    primer commit

---
+++ controllers/usuarios.js
@@ -0,0 +1,55 @@
+const { response, request } = require('express');
+
+
+const usuariosGet = (req = request, res = response) => {
+
+    const { q, nombre, apikey } = req.query;
+
+    res.json({
+        msg: 'get API - controlador',
+       q,
+       nombre,
+       apikey
+    })
+}
+
+const usuariosPut = (req, res) => {
+
+    const id = req.params.id;
+
+    res.json({
+        msg: 'put API - controlador',
+        id
+    })
+}
+
+const usuariosPost = (req, res) => {
+    const body = req.body;
+    const { nombre, edad} = body;
+    res.status(201).json({
+        msg: 'post API - controlador',
+        nombre,
+        edad
+    })
+}
+const usuariosDelete = (req, res) => {
+    res.json({
+        msg: 'delete API - controlador'
+    })
+}
+const usuariosPatch = (req, res) => {
+    res.json({
+        msg: 'patch API - controlador'
+    })
+}
+
+
+
+
+module.exports = {
+    usuariosGet,
+    usuariosPut,
+    usuariosPatch,
+    usuariosDelete,
+    usuariosPost
+}
